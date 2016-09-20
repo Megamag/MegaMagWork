6c040138629ce8c9f886f50721d5a3b3f309baa2
Megamag <Megamag@Compumouse>
2016-09-20 14:55:55 +0300

    List Arr

--- Java/src/megamag/zone/Main.java
+++ Java/src/megamag/zone/Main.java
@@ -1,12 +1,20 @@
 package megamag.zone;
 
 import static java.lang.System.out;
+
+import java.util.ArrayList;
+
 import static java.lang.System.err;
 
 public class Main
 {
+	static ArrayList<String> list = new ArrayList<String>();
+	
 	public static void main(String[] args)
 	{
-		out.println("github.com");
+		err.println(list.size());
+		list.add("Yaacov Rozenbaum 052-3473312");
+		err.println(list.size());
+		out.println(list.get(0));
 	}
 }
