2d787b2cf4538a39dd1bed2a5c7c727e92d40c49
Megamag <Megamag@Compumouse>
2016-09-20 14:39:23 +0300

    Test

---
+++ Java/src/megamag/zone/Main.java
@@ -0,0 +1,12 @@
+package megamag.zone;
+
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		out.println("github.com");
+	}
+}
